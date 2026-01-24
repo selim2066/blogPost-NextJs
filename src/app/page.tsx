de2fbad38617557ca9501b99f8618bd512cf0147
selim2066 <mdselimreza2066@gmail.com>
2026-01-24 19:58:04 +0600

    setup shadcn nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+import {Button} from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="outline" className="outline-green-400 bg-red-400">Click me</Button>
+    </div>
+  )
+}
