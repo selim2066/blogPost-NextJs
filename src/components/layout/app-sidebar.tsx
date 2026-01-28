ce03e4cfb9f1269c6fe4cee48e9796ae7771cce7
selim2066 <mdselimreza2066@gmail.com>
2026-01-28 13:30:00 +0600

    module end 31. alhamdulillah

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -19,27 +19,8 @@ import {adminRoutes} from "@/routes/adminRoutes";
 import {userRoutes} from "@/routes/userRoutes";
 import {Route} from "@/types"
 
-// This is sample data.
-const data = {
-  versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
-  navMain: [
-    {
-      title: "Getting Started",
-      url: "#",
-      items: [
-         {
-          title: "Admin Dashboard",
-          url: "/admin-dashboard",
-        },
-        {
-          title: "User Dashboard",
-          url: "/dashboard",
-        },
-      ],
-    },
-   
-  ],
-};
+
+
 
 export function AppSidebar({user, ...props }:{ user:{role: string} & React.ComponentProps<typeof Sidebar>}) {
 
