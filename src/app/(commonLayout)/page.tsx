acfd2bd0b417cef72f7360083986703f9de5c3af
selim2066 <mdselimreza2066@gmail.com>
2026-02-06 21:43:51 +0600

    handaling params and fetch options

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,24 +1,30 @@
 import BlogCard from "@/components/module/homepage/BlogCard";
-import { Button } from "@/components/ui/button";
+
 import { blogService } from "@/service/blog.service";
 //import { authClient } from "@/lib/auth-client";
 import { userService } from "@/service/user.service";
 import { BlogPost } from "@/types/blogpost";
 
 export default async function Home() {
-
   // const session = await authClient.getSession();
   // console.log("this is session from home and its server side session",session)
 
-  const {data, error}= await userService.getSession();
-  console.log("home(commonlayout page.tsx: data of session and error::n)......",data,error)
+  const { data, error } = await userService.getSession();
+  console.log(
+    "home(commonlayout page.tsx: data of session and error::n)......",
+    data,
+    error,
+  );
 
-  const {postData}= await blogService.getBlogs()
-  console.log("post data.......",postData)
+  const { postData } = await blogService.getBlogs(
+    { isFeatured: true },
+    { cache: "no-store" },
+  );
+
+  
+  console.log("post data.......", postData);
   return (
     <div className="container mx-auto py-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 lg:px-15">
-      {/* <Button>Click MSR</Button> */}
-
       {postData?.data?.map((post: BlogPost) => (
         <BlogCard key={post.id} post={post} />
       ))}
--- src/service/blog.service.ts
+++ src/service/blog.service.ts
@@ -2,10 +2,43 @@ import { env } from "@/env";
 
 const API_URL = env.API_URL;
 
+//* No Dynamic and No { cache: no-store } : SSG -> Static Page
+//* { cache: no-store } : SSR -> Dynamic Page
+//* next: { revalidate: 10 } : ISR -> Mix between static and dynamic
+
+interface GetBlogsParams {
+  isFeatured?: boolean;
+  search?: string;
+}
+interface ServiceOptions {
+  cache?: RequestCache;
+  revalidate?: number;
+}
+
 export const blogService = {
-  getBlogs: async () => {
+  getBlogs: async (params?: GetBlogsParams, options?: ServiceOptions) => {
     try {
-      const res = await fetch(`${API_URL}/post`, { next: { revalidate: 10 } });
+      const url = new URL(`${API_URL}/post`);
+      if (params) {
+        Object.entries(params).forEach(([key, value]) => {
+          if (value !== undefined && value !== null && value !== "") {
+            url.searchParams.append(key, String(value));
+          }
+        });
+      }
+
+      // for config revalidate in fetch
+      const config: RequestInit = {};
+
+      if (options?.revalidate !== undefined) {
+        config.next = { revalidate: options.revalidate };
+      }
+      if (options?.cache) {
+        config.cache = options.cache;
+      }
+      console.log(url.toString());
+      const res = await fetch(url.toString(), config);
+
       const data = await res.json();
       return { postData: data, error: null };
     } catch (error) {
