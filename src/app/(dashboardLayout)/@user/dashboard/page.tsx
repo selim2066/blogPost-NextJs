c15d6dd34093522acb8aa559b9bc1f5860379b3c
selim2066 <mdselimreza2066@gmail.com>
2026-02-15 15:16:55 +0600

    module 35.1 done, dashboard redirect to createBLOGpage

--- src/app/(dashboardLayout)/@user/dashboard/page.tsx
+++ src/app/(dashboardLayout)/@user/dashboard/page.tsx
@@ -1,5 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function UserDashboard() {
-  return (
-    <div>UserDashboard</div>
-  );
+  return redirect("/dashboard/create-blog");
 }
