f5bc33248284ad175564ecfeb77de4688aa3510c
selim2066 <mdselimreza2066@gmail.com>
2026-02-19 16:33:14 +0600

    rename

--- src/app/(dashboardLayout)/@user/dashboard/create-blog/page.tsx
+++ src/app/(dashboardLayout)/@user/dashboard/create-blog/page.tsx
@@ -1,4 +1,4 @@
-import CreateBlogPageServer from "@/components/module/user/createBlog/createBlogPageServer";
+import CreateBlogFormServer from "@/components/module/user/createBlog/createBlogFormServer";
 import CreateBlogFormClient from "@/components/module/user/createBlog/createBlogFormClient";
 import { blogService } from "@/service/blog.service";
 import { BlogPost } from "@/types";
@@ -7,7 +7,7 @@ export default async function CreateBlogPage() {
   const { postData } = await blogService.getBlogPosts({},{cache: "no-store"});
   return (
     <div>
-      {/* <CreateBlogPageServer /> */}
+      {/* <CreateBlogFormServer /> */}
       <CreateBlogFormClient/>
       {/* {postData.data.map((itme: BlogPost) => (
         <div key={itme.id}>
