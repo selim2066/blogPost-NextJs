e03eda9aa371fe6d2f3ee19e8ad4ee36cb9c1741
selim2066 <mdselimreza2066@gmail.com>
2026-02-20 16:33:40 +0600

    pagination thikthak korlam

--- src/app/(dashboardLayout)/@user/dashboard/history/page.tsx
+++ src/app/(dashboardLayout)/@user/dashboard/history/page.tsx
@@ -1,4 +1,5 @@
 import HistoryTable from "@/components/module/user/historyTable/HistoryTable";
+import PaginationControls from "@/components/ui/pagination-control";
 import { blogService } from "@/service/blog.service";
 
 export default async function HistoryPage({searchParams}:{searchParams: Promise<{page: string}>}) {
@@ -6,12 +7,21 @@ export default async function HistoryPage({searchParams}:{searchParams: Promise<
 const response= await blogService.getBlogPosts({page});
 
 const posts = response.postData?.data || [];
+  const pagination = response.postData?.pagination || {
+    limit: 5,
+    page: 1,
+    totalPosts: 0,
+    totalPages: 1,
+  };
 
   return (
     <div>
       <h1 className="text-2xl font-bold mb-6 ml-10 mt-1.5">History Page</h1>
 
       <HistoryTable posts={posts} />
+      <div className="mt-4">
+        <PaginationControls meta={pagination} />
+      </div>
 
     </div>
   );
