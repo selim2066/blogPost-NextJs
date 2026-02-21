7b234fd00d09c87774990a32833154178479f081
selim2066 <mdselimreza2066@gmail.com>
2026-02-21 13:13:25 +0600

    BlogPage thikthak korlam...

--- src/app/(commonLayout)/blogs/page.tsx
+++ src/app/(commonLayout)/blogs/page.tsx
@@ -1,5 +1,23 @@
-export default function BlogsPage() {
+import BlogCard from "@/components/module/homepage/BlogCard";
+import { blogService } from "@/service/blog.service";
+
+import { BlogPost } from "@/types";
+export default async function BlogsPage() {
+  // const featuredPostsPromise = blogService.getBlogPosts({ isFeatured: false });
+  const posts = await blogService.getBlogPosts();
   return (
-    <div>BlogsPagesss</div>
+    <div className="my-10">
+      <div  className="max-w-7xl mx-auto px-4" >
+        <h2 className="text-2xl font-bold mb-6">All Posts</h2>
+        <div className="grid grid-cols-3 gap-5">
+          {posts?.error?.message ? (
+            <p className="text-red-500">{posts?.error?.message}</p>
+          ) : null}
+          {posts?.postData?.data?.map((post: BlogPost) => (
+            <BlogCard key={post.id} post={post} />
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
